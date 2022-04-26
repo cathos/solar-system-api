038b7905ceb5fcb654cad36334f18357dbc84b92
cathos <cathos@startmail.com>
2022-04-26 13:24:52 -0700

    finished wave 1 and 2, submitting part 1

--- app/routes.py
+++ app/routes.py
@@ -10,8 +10,8 @@ class Planet:
 
 planets = [
     Planet(1, 'Mercury', 'The closest planet to the sun! REALLY HOT!', '3.7 m/s2'),
-    Planet(2, 'Venus',  'Another hot planet', '8.87 m/s2'),
-    Planet(3, 'Earth', 'Third Planet from the Sun. Maybe a little special', '9.8 m/s2')
+    Planet(2, 'Venus',  'Another hot planet! Actually hotter than Mercury!', '8.87 m/s2'),
+    Planet(3, 'Earth', 'Third Planet from the Sun. Maybe a little special. Much colder than the first two.', '9.8 m/s2')
 ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
