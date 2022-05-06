455e34a1ebb0d5f08115033b6f5f5493e821e6de
cathos <cathos@startmail.com>
2022-05-06 09:29:36 -0700

    cleaned up after finishing wave 6. still could refactor to clean up models.

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,6 @@ from app import db
 from app.models.planet import Planet
 from flask import Blueprint, jsonify, make_response, request, abort
 
-# class Planet:
-#     def __init__(self, id, name, description, gravity):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.gravity = gravity
-
-# planets = [
-#     Planet(1, 'Mercury', 'The closest planet to the sun! REALLY HOT!', '3.7 m/s2'),
-#     Planet(2, 'Venus',  'Another hot planet! Actually hotter than Mercury!', '8.87 m/s2'),
-#     Planet(3, 'Earth', 'Third Planet from the Sun. Maybe a little special. Much colder than the first two.', '9.8 m/s2')
-# ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
@@ -47,15 +35,6 @@ def read_one_planet(planet_id):
 
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
-    # planets_result = []
-    # for planet in planets:
-    #     planets_result.append(dict(
-    #         id = planet.id,
-    #         name = planet.name,
-    #         description = planet.description,
-    #         gravity = planet.gravity
-    #     ))
-    # return jsonify(planets_result)
     name_query = request.args.get("name")
     description_query = request.args.get("description")
     order_from_sun_query = request.args.get("order_from_sun")
@@ -82,7 +61,11 @@ def get_all_planets():
 @planets_bp.route("", methods=["POST"])
 def add_planet():
     request_body = request.get_json()
-    new_planet = Planet(name=request_body["name"], order_from_sun=request_body["order_from_sun"], description=request_body["description"], gravity=request_body["gravity"])
+    new_planet = Planet(
+        name=request_body["name"], 
+        order_from_sun=request_body["order_from_sun"], 
+        description=request_body["description"], 
+        gravity=request_body["gravity"])
 
     db.session.add(new_planet)
     db.session.commit()
@@ -104,10 +87,6 @@ def update_planet(planet_id):
     if "order_from_sun" in request_body_keys:
         planet.order_from_sun = request_body["order_from_sun"]
 
-    # planet.name = request_body["name"]
-    # planet.description = request_body["description"]
-    # planet.gravity = request_body["gravity"]
-
     db.session.commit()
     return make_response(f"Planet #{planet.id} successfully updated", 200)
 
@@ -120,22 +99,3 @@ def delete_planet(planet_id):
 
     return make_response(f"Planet #{planet.id} successfully deleted", 200)
 
-
-
-# @planets_bp.route("/<planet_id>", methods = ["GET"])
-# def get_planet(planet_id):
-#     try: 
-#         planet_id = int(planet_id)
-#     except:
-#         abort(make_response(jsonify(dict(details=f"planet id {planet_id} invalid")), 400))
-        
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return {
-#                 "id": planet.id,
-#                 "name": planet.name,
-#                 "description": planet.description,
-#                 "gravity": planet.gravity
-                
-#             }
-#     abort(make_response(jsonify(dict(details=f"planet id {planet_id} not found")), 404))
